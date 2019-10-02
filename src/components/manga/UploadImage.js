36cbc4c2f8dfba1094fff1c2cfb9e0f964466dae
Giovanni Salcuni <giovanni.salcuni12@gmail.com>
2019-10-02 16:45:01 +0200

    create the file upload image for tomorrow

---
+++ src/components/manga/UploadImage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import firebase from 'firebase/app'
+import FileUploader from 'react-firebase-file-uploader'
+
+export default class UploadeImage extends React.Component {
+    
+}
