06e4e81ecf512553f9498f3ab869e5d8398294dd
Giovanni Salcuni <giovanni.salcuni12@gmail.com>
2019-10-02 16:30:42 +0200

    create the pages not found

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default class NotFoundPage extends React.Component{
+    render(){
+        return <div>
+            <p style={{textAlign:"center"}}>
+                <p>404 | pages not found</p>
+              <Link to="/">Go to Home </Link>
+            </p>
+          </div>;
+    }
+}
