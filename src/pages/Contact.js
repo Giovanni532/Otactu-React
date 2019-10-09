32a6197b8ff676b66b30c2a44aaf7fbbeb832f17
Giovanni Salcuni <giovanni.salcuni12@gmail.com>
2019-10-09 11:00:20 +0200

    create the form for contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,71 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
+import LoaderCircle from '../loaders/LoaderCircle'
 
 export default class Contact extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            email: "",
+            subject: "",
+            message: "",
+            error: "",
+            loader: false,
+            redirect: false
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event) {
+        const value = event.target.value
+        const name = event.target.name
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        this.submitEmail()
+    }
+
     render() {
+        let classNamError = ""
+
+        if (this.state.error.length > 0) {
+            classNamError = "error"
+        }
+
+        const { redirect } = this.state;
+
+        if (redirect) {
+            return <Redirect to='/home' />;
+        }
+
         return (
-            <h1 style={{ marginTop: 100, textAlign: "center" }}>Contact</h1>
+            <div className="parent-form">
+                <h2 className="title">Contactez-nous</h2>
+                <form onSubmit={this.handleSubmit} className="form">
+                    <label className="label">
+                        votre email
+                        </label>
+                    <input className="input" type="text" name="email" value={this.state.email} onChange={this.handleChange} />
+                    <label className="label">
+                        le sujet
+                        </label>
+                    <input className="input" type="text" name="subject" value={this.state.subject} onChange={this.handleChange} />
+                    <label className="label">
+                        le message
+                        </label>
+                    <input className="input" type="text" name="message" value={this.state.message} onChange={this.handleChange} />
+                    <input className="button" type="submit" value="Envoyer" />
+                </form>
+                {this.state.loader ? <LoaderCircle /> : <p className={classNamError}>{this.state.error}</p>}
+            </div>
         )
     }
 }
