7fb1ab7225e24c3c353b8c01d8af70ba73c72485
Giovanni Salcuni <giovanni.salcuni12@gmail.com>
2019-10-15 10:40:10 +0200

    recup the all data from the redirect for the show

--- src/helpers/Child.js
+++ src/helpers/Child.js
@@ -7,6 +7,9 @@ export const Child = ({ location }) => {
       <p>Manga: {location.state.nameOfManga}</p>
       <p>L'auteur: {location.state.autheur}</p>
       <p>Sortie pour la premiere fois le: {location.state.dateDeSortie}</p>
+      <p>Nombre de saison: {location.state.saison}</p>
+      <p>Le type: {location.state.type}</p>
+      <p>Description: {location.state.description}</p>
     </div>
   );
 }
