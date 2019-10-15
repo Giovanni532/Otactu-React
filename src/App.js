d36d7a2f6c383fc86e3e3e344093884edb27fef4
Giovanni Salcuni <giovanni.salcuni12@gmail.com>
2019-10-15 14:28:07 +0200

    add a new api key for firebase db with push in heroku

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ const configFirebase = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 }
 
 firebase.initializeApp(configFirebase)
