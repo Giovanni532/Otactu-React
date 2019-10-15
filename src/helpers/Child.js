23e3d1f8468e1cbb267dfc2f848398d2053f7835
Giovanni Salcuni <giovanni.salcuni12@gmail.com>
2019-10-15 11:06:06 +0200

    finish the design of the show and the home page user

--- src/helpers/Child.js
+++ src/helpers/Child.js
@@ -3,18 +3,18 @@ import Image from '../assets/gokuKamehaUi.png'
 
 export const Child = ({ location }) => {
   return (
-    <div className="wrapper-profil">
+    <div className="wrapper-manga">
       <div className="image-wrapper">
       <img src={Image} alt="Goku ultra instinct" />
       </div>
-        <div className="mangas-info">
+        <div className="show-wrapper">
           <img className="mangas-info-image" src={location.state.imageUrl} alt={location.state.nameOfManga} />
-          <p className="first-p">Manga: {location.state.nameOfManga}</p>
-          <p>L'auteur: {location.state.autheur}</p>
-          <p>Sortie pour la premiere fois le: {location.state.dateDeSortie}</p>
-          <p>Nombre de saison: {location.state.saison}</p>
-          <p>Le type: {location.state.type}</p>
-          <p>Description: {location.state.description}</p>
+          <p className="manga-info">Manga: {location.state.nameOfManga}</p>
+          <p className="manga-info">L'auteur: {location.state.autheur}</p>
+          <p className="manga-info">Sortie pour la premiere fois le: {location.state.dateDeSortie}</p>
+          <p className="manga-info">Nombre de saison: {location.state.saison}</p>
+          <p className="manga-info">Le type: {location.state.type}</p>
+          <p className="last-manga-info">Description: {location.state.description}</p>
         </div>
     </div>
   );
