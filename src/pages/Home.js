618e872958bd3ebdb2ef7a019ba2305b296c7745
Giovanni Salcuni <giovanni.salcuni12@gmail.com>
2019-10-21 09:13:29 +0200

    redirect in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import {Redirect} from 'react-router-dom'
 
 export default class Home extends React.Component {
     render() {
         return (
-            <h1 style={{ marginTop: 100, textAlign: "center" }}>Home</h1>
+            <Redirect to='/login'/>
         )
     }
 }
