d03ce9e5fb3557a9e029252c8d085f16fb731f70
Giovanni Salcuni <giovanni.salcuni12@gmail.com>
2019-10-21 08:38:34 +0200

    add a design of a congrats screen

--- src/pages/CongratsScreen.js
+++ src/pages/CongratsScreen.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import Image from '../assets/gokuGod.png'
+
 
 const CongratsScreen = ({ location }) => {
     return (
-        <div>
-            <p>Merci {location.state.email}</p>
-            <p>Ton email a bien ete envoyer</p>
+        <div className="wrapper-form">
+            <div>
+                <img style={{ height: 350 }} src={Image} className="image-form" alt="goku super sayan god" />
+            </div>
+            <div className="congrats">
+                <p>Merci {location.state.email}, ton email a bien ete envoyer !</p>
+                <Link to='/home'>Retour</Link>
+            </div>
         </div>
     )
 }
